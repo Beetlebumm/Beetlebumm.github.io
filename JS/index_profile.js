60c42abb79d6ca006b2916827910ef41a04de53e
Allanchu <50914938+Beetlebumm@users.noreply.github.com>
2021-07-10 13:31:18 +0800

    Cute Guy

---
+++ JS/index_profile.js
@@ -0,0 +1,25 @@
+function preload() {
+    profile_img = loadImage("../image/index/profile.png");
+
+}
+
+function setup() {
+    createCanvas(displayWidth / 4, displayHeight / 1.5, WEBGL);
+    angleMode(DEGREES);
+
+}
+
+function draw() {
+    background(0);
+    background(244, 243, 255);
+    rotateY(180 + (mouseX - width) / width * 8);
+
+    rotateX(0 + (mouseY - height / 2) / height * 8);
+
+    texture(profile_img);
+
+    noStroke;
+
+    sphere(width / 4, 25, 6);
+
+}
